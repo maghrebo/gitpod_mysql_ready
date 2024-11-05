5d604757830fa89ec040a8203b0b0cae9a64f5a2
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-11-05 10:41:35 +0000

    parzialmente completato pure l'esercizio 05

---
+++ esercizio-03-preparazione-verifica/first-rest-api.py
@@ -0,0 +1,95 @@
+import mysql.connector
+from flask import Flask
+
+#Connect to mysql
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="Animali"
+)
+
+mycursor = mydb.cursor()
+
+app = Flask(__name__)
+
+def getAllData():
+    mycursor.execute("SELECT * FROM mammiferi")
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
+
+@app.route("/")
+def stampaDati():
+    data = getAllData()
+    return ({"Mammiferi: " : data})
+
+# Da modificare personlizzando per il mio database degli animali (mammiferi)
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route("/getAllDataInHtml")
+def getAllDataClash():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit")
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
+
+@app.route("/air_transport")
+def airTransport():
+    condizione = "Air"
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit where Transport = %s", (condizione,))
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
+
+@app.route("/epic_units")
+def epicUnits():
+    condizione = "Epic"
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit where Rarity = %s", (condizione,))
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
+
+@app.route("/count_x2")
+def countX2():
+    condizione = "x2"
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit where Count = %s", (condizione,))
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
+
+@app.route("/target_ground")
+def targetGround():
+    condizione = "Ground"
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit where Target = %s", (condizione,))
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
