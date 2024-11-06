630fb0c7f9ab08d423a9c5cda5b6de92fb34e9af
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-11-06 12:56:48 +0000

    mezzo finito

--- esercizio-03-preparazione-verifica/first-rest-api.py
+++ esercizio-03-preparazione-verifica/first-rest-api.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from flask import Flask, jsonify
 
-#Connect to mysql
+#Connessione al database
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -13,6 +13,8 @@ mycursor = mydb.cursor()
 
 app = Flask(__name__)
 
+#Definizione delle diverse funzioni per il metodo GET
+
 def getAllData():
     mycursor.execute("SELECT * FROM mammiferi")
     myresult = mycursor.fetchall()
@@ -22,14 +24,14 @@ def getAllData():
         result.append(x);
     return result
 
-def getRazza():
+def getRazza(razza):
     query = "SELECT * FROM mammiferi WHERE razza = %s"
     mycursor.execute(query, (razza,))
-    result = [];
-    for x in myresult:
-        print(x);
-        result.append(x);
-    return result
+    rows = mycursor.fetchall()
+    return rows
+
+
+#Indirizzamento delle diverse route che poi richiamano le funzioni
 
 @app.route("/")
 def stampaDati():
@@ -39,73 +41,10 @@ def stampaDati():
 @app.route("/<razza>")
 def stampaRazza(razza):
     data = getRazza(razza)
-    return jsonify({"Mammiferi": data})
+    return jsonify({"La razza scelta e: ": data})
 
 
+#-----------------------------------------------------------------
 
-# Da modificare personlizzando per il mio database degli animali (mammiferi)
-
-
-
-
-
-
-
-
-
-
-
-
-@app.route("/getAllDataInHtml")
-def getAllDataClash():
-    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit")
-    myresult = mycursor.fetchall()
-    result = [];
-    for x in myresult:
-        print(x);
-        result.append(x);
-    return result
-
-@app.route("/air_transport")
-def airTransport():
-    condizione = "Air"
-    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit where Transport = %s", (condizione,))
-    myresult = mycursor.fetchall()
-    result = [];
-    for x in myresult:
-        print(x);
-        result.append(x);
-    return result
-
-@app.route("/epic_units")
-def epicUnits():
-    condizione = "Epic"
-    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit where Rarity = %s", (condizione,))
-    myresult = mycursor.fetchall()
-    result = [];
-    for x in myresult:
-        print(x);
-        result.append(x);
-    return result
-
-@app.route("/count_x2")
-def countX2():
-    condizione = "x2"
-    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit where Count = %s", (condizione,))
-    myresult = mycursor.fetchall()
-    result = [];
-    for x in myresult:
-        print(x);
-        result.append(x);
-    return result
-
-@app.route("/target_ground")
-def targetGround():
-    condizione = "Ground"
-    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit where Target = %s", (condizione,))
-    myresult = mycursor.fetchall()
-    result = [];
-    for x in myresult:
-        print(x);
-        result.append(x);
-    return result
+def addAnimale():
+    query = ""
